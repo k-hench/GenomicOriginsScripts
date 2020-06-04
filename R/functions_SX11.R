2aa37b69b776f6c5a4b37bb03a8e569ada06d34f
k-hecnh <khench@posteo.net>
2020-06-04 09:41:45 +0200

    update

---
+++ R/functions_SX11.R
@@ -0,0 +1,14 @@
+#' Get gxp data
+#'
+#' \code{get_gxp_both_models} imports gxp data
+#'
+#' @family Figure SX
+#'
+#' @export
+get_gxp_both_models <- function(file, trait, model_type, path){
+  vroom::vroom(str_c(path,file), delim = "\t") %>%
+    left_join(hypo_chrom_start)  %>%
+    mutate(trait = trait,
+           model_type = model_type,
+           gpos = GSTART + MID_POS)
+}
