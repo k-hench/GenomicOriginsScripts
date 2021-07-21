9fd954ff5ced2caadda59e08c0dabb880d67199a
k-hench <khench@posteo.net>
2021-07-21 13:15:07 +0200

    remove example for RMD check

--- R/data.R
+++ R/data.R
@@ -5,8 +5,4 @@
 #' @format A ggtree object with a generic hamlet image:
 
 #' @source KH drawing
-#'
-#' @examples
-#' ggplot2::ggplot() +
-#'  ggplot2::annotation_custom(grob = GenomicOriginsScripts::generic_hamlet_img)
 "generic_hamlet_img"
